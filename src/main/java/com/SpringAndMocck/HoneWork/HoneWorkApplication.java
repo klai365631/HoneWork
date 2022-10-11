cafa70aa4e076e385cfd582ebee57959935ef997
ANDRE <klai1@mail.ru>
2022-10-11 12:01:04 +0400

    Initial commit

---
+++ src/main/java/com/SpringAndMocck/HoneWork/HoneWorkApplication.java
@@ -0,0 +1,13 @@
+package com.SpringAndMocck.HoneWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringAndMocck/HoneWork/HoneWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringAndMocck.HoneWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
